0b3c4ebf806d0c59bcc80bc8b5ae658b13ad7c72
HNKJXY <344376588@qq.com>
2018-03-22 14:54:41 +0800

    完成功能1

--- app/src/main/java/zzxty/com/gitprotest/MainActivity.java
+++ app/src/main/java/zzxty/com/gitprotest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //推送到远程仓库
+        //完成功能1
     }
 }
